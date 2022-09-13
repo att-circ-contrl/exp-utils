2398501d6fe342d57b9276ab0e0cc96f056b6039
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2022-09-13 11:13:12 -0500

    Moved low-level file search functions from ExpUtils to LoopUtil.

--- libraries/lib-exputils-util/euUtil_getExperimentFolders.m
+++ libraries/lib-exputils-util/euUtil_getExperimentFolders.m
@@ -27,59 +27,15 @@ dirs_use = {};
 % Search the tree.
 
 if isdir(topdir)
-  dirs_opene = helper_searchForFile(topdir, 'structure.oebin');
-  dirs_intanrec = helper_searchForFile(topdir, 'info.rhd');
-  dirs_intanstim = helper_searchForFile(topdir, 'info.rhs');
+  [ scratch dirs_opene ] = nlIO_searchForFile(topdir, 'structure.oebin');
+  [ scratch dirs_intanrec ] = nlIO_searchForFile(topdir, 'info.rhd');
+  [ scratch dirs_intanstim ] = nlIO_searchForFile(topdir, 'info.rhs');
 
-  dirs_use = helper_searchForDir(topdir, 'RuntimeData');
+  dirs_use = nlIO_searchForDir(topdir, 'RuntimeData');
 end
 
 
 % Done.
-
-end
-
-
-%
-% Helper Functions
-
-
-% This looks for a file and returns a path, without the target file's name.
-
-function dirs_found = helper_searchForFile( startdir, targetname )
-
-  dirs_found = {};
-
-  dirlist = dir([ startdir filesep '**' filesep targetname ]);
-
-  for didx = 1:length(dirlist)
-    thisentry = dirlist(didx);
-    thisfile = thisentry.folder;
-    dirs_found = [ dirs_found {thisfile} ];
-  end
-
-end
-
-
-% This looks for a folder and returns a path, including the target folder.
-
-function dirs_found = helper_searchForDir( startdir, targetname )
-
-  dirs_found = {};
-
-  % NOTE - This only lists the _contents_ of the target, not the target
-  % itself. So, we'll get one or more entries with the target as the path,
-  % for each matching target.
-
-  dirlist = dir([ startdir filesep '**' filesep targetname ]);
-
-  % Everything in the array should be a valid match.
-  if ~isempty(dirlist)
-    dirs_found = [ dirs_found { dirlist.folder } ];
-  end
-
-  % Reduce to only one entry per match.
-  dirs_found = unique(dirs_found);
 
 end
 
