dc79919d26b634aa98d6040b0359bc4e7d5b8104
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2023-04-17 11:19:31 -0500

    Added high-level Chris-specific box plot helper. Formatted whitespace on low-level helper.

---
+++ libraries/lib-exputils-chris/euChris_plotCaseDataBoxes.m
@@ -0,0 +1,147 @@
+function euChris_plotCaseDataBoxes( ...
+  casedata, plotpercase, yfield, xfield, ...
+  casetitles, caselabels, plottype, xstr, ystr, ...
+  titleprefix, titlesuffix, fnameprefix, fnamesuffix )
+
+% function euChris_plotCaseDataBoxes( ...
+%   casedata, plotpercase, yfield, xfield, ...
+%   casetitles, caselabels, plottype, xstr, ystr, ...
+%   titleprefix, titlesuffix, fnameprefix, fnamesuffix )
+%
+% This makes a series of box plots with individual and aggregated data.
+%
+% This is intended to work with the output of "euChris_evalXXX" functions,
+% which generate structures with various statistics stored as fields. This
+% functiton is passed a cell array of such structures (for different
+% experiment cases), and plots individual and aggregate statistics.
+%
+% "casedata" is a cell array containing per-case structures with data.
+% "plotpercase" is true if per-case plots are to be generated in additon to
+%   combined/aggregated plots.
+% "yfield" is the structure field name containing data values to plot.
+% "xfield" is the structure field name containing per-sample category/bin
+%   values/labels. This is a vector for numeric values and a cell array for
+%   labels.
+% "casetitles" is a cell array containing plot-safe human-readable case titles.
+% "caselabels" is a cell array containing filename-safe per-case labels.
+% "plottype" is a label giving hints about how to set up the plot. Use '' for
+%   defaults. Known values are: 'tortemagrel', 'tortemagabs', 'tortphase'
+% "xstr" is a character vector containing the X axis label.
+% "ystr" is a character vector containing the Y axis label.
+% "titleprefix" is a character vector with plot-safe text to add before the
+%   case title (including whitespace and delimiters).
+% "titlesuffix" is a character vector with plot-safe text to append after the
+%   case title (including whitespace and delimiters).
+% "fnameprefix" is a character vector with filename-safe text to add before
+%   the case label.
+% "fnamesuffix" is a character vector with filename-safe text to append after
+%   the case label.
+%
+% No return value.
+
+
+% Figure out plot details depending on the type hint.
+
+xtype = 'linear';
+ytype = 'linear';
+xrange = [];
+yrange = [];
+legendpos = 'off';
+boxwidth = 0.5;
+wantoutliers = true;
+plotlines = {};
+plothcursors = {};
+
+cols = nlPlot_getColorPalette();
+
+if strcmp(plottype, 'tortemagrel')
+  xtype = 'linear';
+  ytype = 'log';
+  xrange = [ 0 4 ];
+  yrange = [ 0.2 200 ];
+  boxwidth = 0.2;
+  plothcursors = { { 1 cols.gry '' } };
+elseif strcmp(plottype, 'tortemagabs')
+  xtype = 'linear';
+  ytype = 'linear';
+  xrange = [ 0 4 ];
+  yrange = [ 0 10 ];
+  boxwidth = 0.2;
+  plotlines = { { 0 0 4 4 cols.gry '' } };
+elseif strcmp(plottype, 'tortephaseerr')
+  yrange = [ -200 200 ];
+  xrange = [ -200 200 ];
+  boxwidth = 30;
+  plothcursors = { { 0 cols.gry '' } };
+end
+
+
+% Per-case plots.
+% Build the aggregate data here.
+
+aggrydata = [];
+aggrxdata = [];
+aggrlabels = {};
+
+for cidx = 1:length(caselabels)
+
+  thiscaselabel = caselabels{cidx};
+  thiscasetitle = casetitles{cidx};
+  thiscasedata = casedata{cidx};
+  thisydata = thiscasedata.(yfield);
+  thisxdata = thiscasedata.(xfield);
+
+  if ~isrow(thisydata)
+    thisydata = transpose(thisydata);
+  end
+  if ~isrow(thisxdata)
+    thisxdata = transpose(thisxdata);
+  end
+
+  aggrydata = [ aggrydata thisydata ];
+  aggrxdata = [ aggrxdata thisxdata ];
+
+  thislabelvec = {};
+  % FIXME - Maybe use titles?
+  thislabelvec(1:length(thisydata)) = { thiscaselabel };
+  aggrlabels = [ aggrlabels thislabelvec ];
+
+
+  % Only generate per-case plots if we were asked to.
+
+  if plotpercase
+    titlestr = [ titleprefix thiscasetitle titlesuffix ];
+    fname = [ fnameprefix thiscaselabel fnamesuffix ];
+
+    euPlot_plotMultipleBoxCharts( thisydata, thisxdata, {}, ...
+      xtype, ytype, xrange, yrange, xstr, ystr, ...
+      wantoutliers, boxwidth, plotlines, plothcursors, 'off', ...
+      titlestr, fname );
+  end
+
+end
+
+
+% Aggregate plots.
+% One with true aggregate data, one showing the per-case data in one plot.
+
+euPlot_plotMultipleBoxCharts( aggrydata, aggrxdata, {}, ...
+  xtype, ytype, xrange, yrange, xstr, ystr, ...
+  wantoutliers, boxwidth, plotlines, plothcursors, 'off', ...
+  [ titleprefix 'Aggregate' titlesuffix ], ...
+  [ fnameprefix 'aggregate' fnamesuffix ] );
+
+euPlot_plotMultipleBoxCharts( aggrydata, aggrxdata, aggrlabels, ...
+  xtype, ytype, xrange, yrange, xstr, ystr, ...
+  wantoutliers, boxwidth, plotlines, plothcursors, 'northeast', ...
+  [ titleprefix 'Combined' titlesuffix ], ...
+  [ fnameprefix 'all' fnamesuffix ] );
+
+
+% Done.
+end
+
+
+
+%
+% This is the end of the file.
--- libraries/lib-exputils-plot/euPlot_plotMultipleBoxCharts.m
+++ libraries/lib-exputils-plot/euPlot_plotMultipleBoxCharts.m
@@ -40,186 +40,167 @@ function euPlot_plotMultipleBoxCharts( ...
 % "titlestr" is a character vector with the plot title.
 % "fname" is the filename to save the plot to.
 
-  thisfig = figure();
-  figure(thisfig);
-  clf('reset');
+
+thisfig = figure();
+figure(thisfig);
+clf('reset');
 
 
-  % Figure out if we're separating bins or datasets.
+% Figure out if we're separating bins or datasets.
 
-  have_by_bin = ~isempty(databinvalues);
-  have_by_set = ~isempty(datasetlabels);
-
-
-  % Call "boxchart" one or more times.
-
-  % If we have bins but no datasets, make one call. All of the boxes are the
-  % same colour.
-
-  % We have datasets but no bins, we still have to make multiple calls
-  % manually to get our desired colour palette.
-
-  % If we have datasets _and_ bins, make multiple calls manually, and perturb
-  % the bin values by an appropriate fraction of the box width on each call.
-  % Use a legend to show the different datasets.
-
-  % Using "GroupByColor" gives really bad placement, so we have to do this
-  % manually. It also forces a default colour palette.
+have_by_bin = ~isempty(databinvalues);
+have_by_set = ~isempty(datasetlabels);
 
 
-  if have_by_set
-    setlabels = unique(datasetlabels);
-    setcount = length(setlabels);
-    % Use brighter/more sturated colours.
-    setcolours = nlPlot_getColorSpread( [ 0.9 0 0 ], setcount, 240 );
+% Call "boxchart" one or more times.
 
-    setoffsets = 0;
-    gapcount = setcount - 1;
-    if gapcount > 0
-      setoffsets = 0:gapcount;
-      setoffsets = (setoffsets / gapcount) - 0.5;
-    end
+% If we have bins but no datasets, make one call. All of the boxes are the
+% same colour.
 
-    setoffsets = setoffsets * boxwidth;
-    if setcount > 1
-      boxwidth = boxwidth / setcount;
-      boxwidth = boxwidth * 0.9;
-    end
+% We have datasets but no bins, we still have to make multiple calls
+% manually to get our desired colour palette.
+
+% If we have datasets _and_ bins, make multiple calls manually, and perturb
+% the bin values by an appropriate fraction of the box width on each call.
+% Use a legend to show the different datasets.
+
+% Using "GroupByColor" gives really bad placement, so we have to do this
+% manually. It also forces a default colour palette.
+
+
+if have_by_set
+  setlabels = unique(datasetlabels);
+  setcount = length(setlabels);
+  % Use brighter/more sturated colours.
+  setcolours = nlPlot_getColorSpread( [ 0.9 0 0 ], setcount, 240 );
+
+  setoffsets = 0;
+  gapcount = setcount - 1;
+  if gapcount > 0
+    setoffsets = 0:gapcount;
+    setoffsets = (setoffsets / gapcount) - 0.5;
   end
 
+  setoffsets = setoffsets * boxwidth;
+  if setcount > 1
+    boxwidth = boxwidth / setcount;
+    boxwidth = boxwidth * 0.9;
+  end
+end
 
-  % Render the box plots.
 
-  if have_by_set
+% Render the box plots.
 
-    % One plot per dataset.
-    % If we have no bins, use dataset labels.
-    % If we have both bins and datasets, use a legend.
+if have_by_set
 
-    hold on;
+  % One plot per dataset.
+  % If we have no bins, use dataset labels.
+  % If we have both bins and datasets, use a legend.
 
-    for cidx = 1:setcount
+  hold on;
 
-      thislabel = setlabels{cidx};
-      thiscol = setcolours{cidx};
+  for cidx = 1:setcount
 
-      thismask = strcmp(datasetlabels, thislabel);
-      thisdata = datavalues(thismask);
+    thislabel = setlabels{cidx};
+    thiscol = setcolours{cidx};
 
-      if have_by_bin
-        thisbinvalues = databinvalues(thismask);
-        thisbinvalues = thisbinvalues + setoffsets(cidx);
-      else
-        thisbinvalues = {};
-        thisbinvalues(1:length(thisdata)) = { thislabel };
-        thisbinvalues = categorical(thisbinvalues);
-      end
+    thismask = strcmp(datasetlabels, thislabel);
+    thisdata = datavalues(thismask);
 
-      if wantoutliers
-        boxchart( thisbinvalues, thisdata, 'Boxwidth', boxwidth, ...
-          'BoxFaceColor', thiscol, 'MarkerColor', thiscol, ...
-          'DisplayName', thislabel, ...
-          'Markerstyle', '.', 'JitterOutliers', 'on' );
-      else
-        boxchart( thisbinvalues, thisdata, 'Boxwidth', boxwidth, ...
-          'BoxFaceColor', thiscol, 'DisplayName', thislabel, ...
-          'Markerstyle', 'none' );
-      end
-
-    end
-
-    hold off;
-
-  else
-
-    % No datasets; just one plot, with numeric bin values.
-
-    if ~have_by_bin
-      % Internally we get ordinal values (or a single value of "1") by
-      % default, so just do that manually here.
-      databinvalues = ones(size(datavalues));
+    if have_by_bin
+      thisbinvalues = databinvalues(thismask);
+      thisbinvalues = thisbinvalues + setoffsets(cidx);
+    else
+      thisbinvalues = {};
+      thisbinvalues(1:length(thisdata)) = { thislabel };
+      thisbinvalues = categorical(thisbinvalues);
     end
 
     if wantoutliers
-      boxchart( databinvalues, datavalues, 'Boxwidth', boxwidth, ...
+      boxchart( thisbinvalues, thisdata, 'Boxwidth', boxwidth, ...
+        'BoxFaceColor', thiscol, 'MarkerColor', thiscol, ...
+        'DisplayName', thislabel, ...
         'Markerstyle', '.', 'JitterOutliers', 'on' );
     else
-      boxchart( databinvalues, datavalues, 'Boxwidth', boxwidth, ...
+      boxchart( thisbinvalues, thisdata, 'Boxwidth', boxwidth, ...
+        'BoxFaceColor', thiscol, 'DisplayName', thislabel, ...
         'Markerstyle', 'none' );
     end
 
   end
 
+  hold off;
 
-  % If we were asked for lines or cursors, draw them.
+else
 
-  hold on;
+  % No datasets; just one plot, with numeric bin values.
 
-
-  % Figure out what our X limits are, and whether we have a categorical axis.
-
-  axis_is_categories = have_by_set && (~have_by_bin);
-
-  % If we have no size information at all, we're centered on 1.
-  xmin = 0;
-  xmax = 2;
-
-  if have_by_bin
-    if isempty(xrange)
-      % Span the data bin range, plus a bit.
-
-      xmin = min(databinvalues);
-      xmax = max(databinvalues);
-
-      xpad = 1;
-      if xmax > xmin
-        xpad = 0.1 * (xmax - xmin);
-        xmin = xmin - xpad;
-        xmax = xmax + xpad;
-      end
-    else
-      % Span the plot range.
-      xmin = min(xrange);
-      xmax = max(xrange);
-    end
-  elseif axis_is_categories
-    % Our values are categorical, not numeric.
-    setcats = categorical(datasetlabels);
-    xmin = setcats(1);
-    xmax = setcats(length(setcats));
+  if ~have_by_bin
+    % Internally we get ordinal values (or a single value of "1") by
+    % default, so just do that manually here.
+    databinvalues = ones(size(datavalues));
   end
 
-
-  % Draw lines if we're _not_ categorical.
-
-  if ~axis_is_categories
-    for lidx = 1:length(plotlines)
-      thislinedef = plotlines{lidx};
-
-      thisxseries = [ thislinedef{1} thislinedef{3} ];
-      thisyseries = [ thislinedef{2} thislinedef{4} ];
-      thiscol = thislinedef{5};
-      thislabel = thislinedef{6};
-
-      if isempty(thislabel)
-        plot( thisxseries, thisyseries, 'Color', thiscol, ...
-          'HandleVisibility', 'off' );
-      else
-        plot( thisxseries, thisyseries, 'Color', thiscol, ...
-          'DisplayName', thislabel );
-      end
-    end
+  if wantoutliers
+    boxchart( databinvalues, datavalues, 'Boxwidth', boxwidth, ...
+      'Markerstyle', '.', 'JitterOutliers', 'on' );
+  else
+    boxchart( databinvalues, datavalues, 'Boxwidth', boxwidth, ...
+      'Markerstyle', 'none' );
   end
 
-  % Draw horizontal cursors no matter what.
+end
 
-  for lidx = 1:length(plothcursors)
-    thislinedef = plothcursors{lidx};
 
-    thisxseries = [ xmin xmax ];
-    thisyseries = [ thislinedef{1} thislinedef{1} ];
-    thiscol = thislinedef{2};
-    thislabel = thislinedef{3};
+% If we were asked for lines or cursors, draw them.
+
+hold on;
+
+
+% Figure out what our X limits are, and whether we have a categorical axis.
+
+axis_is_categories = have_by_set && (~have_by_bin);
+
+% If we have no size information at all, we're centered on 1.
+xmin = 0;
+xmax = 2;
+
+if have_by_bin
+  if isempty(xrange)
+    % Span the data bin range, plus a bit.
+
+    xmin = min(databinvalues);
+    xmax = max(databinvalues);
+
+    xpad = 1;
+    if xmax > xmin
+      xpad = 0.1 * (xmax - xmin);
+      xmin = xmin - xpad;
+      xmax = xmax + xpad;
+    end
+  else
+    % Span the plot range.
+    xmin = min(xrange);
+    xmax = max(xrange);
+  end
+elseif axis_is_categories
+  % Our values are categorical, not numeric.
+  setcats = categorical(datasetlabels);
+  xmin = setcats(1);
+  xmax = setcats(length(setcats));
+end
+
+
+% Draw lines if we're _not_ categorical.
+
+if ~axis_is_categories
+  for lidx = 1:length(plotlines)
+    thislinedef = plotlines{lidx};
+
+    thisxseries = [ thislinedef{1} thislinedef{3} ];
+    thisyseries = [ thislinedef{2} thislinedef{4} ];
+    thiscol = thislinedef{5};
+    thislabel = thislinedef{6};
 
     if isempty(thislabel)
       plot( thisxseries, thisyseries, 'Color', thiscol, ...
@@ -229,46 +210,69 @@ function euPlot_plotMultipleBoxCharts( ...
         'DisplayName', thislabel );
     end
   end
+end
 
+% Draw horizontal cursors no matter what.
 
-  % Finished with lines and cursors.
+for lidx = 1:length(plothcursors)
+  thislinedef = plothcursors{lidx};
 
-  hold off;
+  thisxseries = [ xmin xmax ];
+  thisyseries = [ thislinedef{1} thislinedef{1} ];
+  thiscol = thislinedef{2};
+  thislabel = thislinedef{3};
 
-
-  % Set up axes and add decorations.
-
-  if have_by_bin && (~isempty(xrange))
-    xlim(xrange);
+  if isempty(thislabel)
+    plot( thisxseries, thisyseries, 'Color', thiscol, ...
+      'HandleVisibility', 'off' );
   else
-    xlim('auto');
+    plot( thisxseries, thisyseries, 'Color', thiscol, ...
+      'DisplayName', thislabel );
   end
-  if ~isempty(yrange)
-    ylim(yrange);
-  else
-    ylim('auto');
-  end
+end
 
-  if have_by_bin && strcmp(xtype, 'log');
-    set(gca, 'xscale', 'log');
-  end
-  if strcmp(ytype, 'log');
-    set(gca, 'yscale', 'log');
-  end
 
-  if have_by_set && (~strcmp(legendpos, 'off'))
-    legend('Location', legendpos);
-  else
-    legend('off');
-  end
+% Finished with lines and cursors.
 
-  title(titlestr);
-  xlabel(xstr);
-  ylabel(ystr);
+hold off;
 
-  saveas(thisfig, fname);
 
-  close(thisfig);
+% Set up axes and add decorations.
+
+if have_by_bin && (~isempty(xrange))
+  xlim(xrange);
+else
+  xlim('auto');
+end
+if ~isempty(yrange)
+  ylim(yrange);
+else
+  ylim('auto');
+end
+
+if have_by_bin && strcmp(xtype, 'log');
+  set(gca, 'xscale', 'log');
+end
+if strcmp(ytype, 'log');
+  set(gca, 'yscale', 'log');
+end
+
+if have_by_set && (~strcmp(legendpos, 'off'))
+  legend('Location', legendpos);
+else
+  legend('off');
+end
+
+title(titlestr);
+xlabel(xstr);
+ylabel(ystr);
+
+saveas(thisfig, fname);
+
+close(thisfig);
+
+
+% Done.
 end
 
 
