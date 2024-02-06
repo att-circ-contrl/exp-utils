56526f53b3fd8dbacc43943946bf6381a17af953
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-02-06 09:57:06 -0600

    Added high-level time alignment functions.

---
+++ libraries/lib-exputils-highlevel/euHLev_alignAllDevices.m
@@ -0,0 +1,115 @@
+function timetables = euHLev_alignAllDevices( ...
+  boxevents, gameevents, ttlevents, gameframedata )
+
+% function timetables = euHLev_alignAllDevices( ...
+%   boxevents, gameevents, ttlevents, gameframedata )
+%
+% This makes several calls to euUSE_AlignTwoDevices() to build tables of
+% corresponding timestamps for SyncBox, USE, ephys, and eye-tracker devices.
+%
+% This is intended to work with the output of euHLev_readAllTTLEvents() and
+% euUSE_readRawFrameData().
+%
+% NOTE - This assumes that _either_ Open Ephys _or_ Intan recorder output
+% is supplied, not both at once.
+%
+% Supplied event structures may be struct([]) for devices that are absent.
+%
+% "boxevents" is a structure containing SynchBox event data tables.
+% "gameevents" is a structure containing USE event data tables.
+% "ttlevents" is a structure containing structures with event tables from
+%   several devices:
+%   "openephys" contains event tables from Open Ephys.
+%   "intanrec" contains event tables from the Intan recording controller.
+%   "intanstim" contains event tables from the Intan stimulation controller.
+% "gameframedata" is a FrameData data table.
+%
+% "timetables" is a structure containing several tables with corresponding
+%   time tuples (empty tables if alignment couldn't be performed):
+%   "record_synchbox" associates 'synchBoxTime' and 'recTime'.
+%   "record_game" associates 'unityTime' and 'recTime'.
+%   "record_stim" associates 'recTime' and 'stimTime'.
+%   "game_eye" associates 'unityTime' and 'eyeTime'.
+
+
+timetables = struct();
+timetables.record_synchbox = table();
+timetables.record_game = table();
+timetables.record_stim = table();
+timetables.game_eye = table();
+
+
+% Figure out which recording device we're using.
+
+recevents = ttlevents.openephys;
+if isempty(recevents)
+  recevents = ttlevents.intanrec;
+end
+
+% Copy the stimulator for convenience.
+stimevents = ttlevents.intanstim;
+
+
+% NOTE - We're only aligning on event codes.
+% Using reward pulses takes too long.
+
+
+% For SynchBox alignment, use raw code bytes rather than cooked codes.
+% Otherwise we get glitching from missing codes in the serial reply stream.
+
+if isfield(recevents, 'rawcodes') && isfield(boxevents, 'rawcodes')
+  disp('.. Aligning recorder and SynchBox timestamps.');
+
+  [ scratch timelut ] = euUSE_alignTwoDevices( ...
+    { recevents.rawcodes, boxevents.rawcodes }, ...
+    'recTime', 'synchBoxTime', struct() );
+
+  timetables.record_synchbox = timelut;
+
+  disp('.. Finished aligning recorder and SynchBox timestamps.');
+end
+
+
+if isfield(recevents, 'cookedcodes') && isfield(gameevents, 'cookedcodes')
+  disp('.. Aligning recorder and game timestamps.');
+
+  [ scratch timelut ] = euUSE_alignTwoDevices( ...
+    { recevents.cookedcodes, gameevents.cookedcodes }, ...
+    'recTime', 'unityTime', struct() );
+
+  timetables.record_game = timelut;
+
+  disp('.. Finished aligning recorder and game timestamps.');
+end
+
+
+if isfield(recevents, 'cookedcodes') && isfield(stimevents, 'cookedcodes')
+  disp('.. Aligning recorder and stimulator timestamps.');
+
+  [ scratch timelut ] = euUSE_alignTwoDevices( ...
+    { recevents.cookedcodes, stimevents.cookedcodes }, ...
+    'recTime', 'stimTime', struct() );
+
+  timetables.record_stim = timelut;
+
+  disp('.. Finished aligning recorder and stimulator timestamps.');
+end
+
+
+if ~isempty(gameframedata)
+  disp('.. Aligning game and eye-tracker timestamps.');
+
+  % We're given tuples explicitly here.
+  timetables.game_eye = euAlign_getUniqueTimestampTuples( ...
+    gameframedata, {'unityTime', 'eyeTime'} );
+
+  disp('.. Finished aligning game and eye-tracker timestamps.');
+end
+
+
+% Done.
+end
+
+
+%
+% This is the end of the file.
---
+++ libraries/lib-exputils-highlevel/euHLev_propagateAlignedTimestamps.m
@@ -0,0 +1,125 @@
+function [ newboxevents newgameevents newttlevents newgameframedata ...
+  newgamegazedata ] = euHLev_propagateAlignedTimestamps( ...
+    timetables, oldboxevents, oldgameevents, oldttlevents, ...
+    oldgameframedata, oldgamegazedata )
+
+% function [ newboxevents newgameevents newttlevents newgameframedata ...
+%   newgamegazedata ] = euHLev_propagateAlignedTimestamps( ...
+%     timetables, oldboxevents, oldgameevents, oldttlevents, ...
+%     oldgameframedata, oldgamegazedata )
+%
+% This uses the time correspondence lookup tables from euHLev_alignAllDevices
+% to propagate timestamps from different devices between event lists returned
+% by euHLev_readAllTTLEvents and the USE FrameData and GazeData tables.
+%
+% Supplied event structures may be struct([]) for devices that are absent.
+% Supplied time lookup tables may be empty for devices that couldn't be
+% aligned (or were absent).
+%
+% "timetables" is a structure containing several tables whose rows contain
+%   corresponding timestamps from different devices:
+%   "record_synchbox" associates 'synchBoxTime' and 'recTime'.
+%   "record_game" associates 'unityTime' and 'recTime'.
+%   "record_stim" associates 'recTime' and 'stimTime'.
+%   "game_eye" associates 'unityTime' and 'eyeTime'.
+% "oldboxevents" is a structure containing SynchBox event data tables.
+% "oldgameevents" is a structure containing USE event data tables.
+% "oldttlevents" is a structure cotnaining structures with event tables from
+%   several devices:
+%   "openephys" contains event tables from Open Ephys.
+%   "intanrec" contains event tables from the Intan recording controller.
+%   "intanstim" contains event tables from the Intan stimulation controller.
+% "oldgameframedata" is a USE FrameData data table.
+% "oldgamegazedata" is a USE GazeData data table.
+%
+% "newboxevents" is a copy of "oldboxevents" with new timestamp columns.
+% "newgameevents" is a copy of "oldgameevents" with new timestamp columns.
+% "newttlevents" is a copy of "oldttlevents" with new timestamp columns.
+% "newgameframedata" is a copy of "oldgameframedata" with new time columns.
+% "newgamegazedata" is a copy of "oldgamegazedata" with new time columns.
+
+
+% Initialize.
+
+newboxevents = oldboxevents;
+newgameevents = oldgameevents;
+newttlevents = oldttlevents;
+newgameframedata = oldgameframedata;
+newgamegazedata = oldgamegazedata;
+
+
+
+% Add recorder timstamps to everything we aligned it with.
+% We know that these tables are non-empty if we did alignment with them.
+
+if ~isempty(timetables.record_synchbox)
+  newboxevents = euAlign_addTimesToAllTables( ...
+    oldboxevents, 'synchBoxTime', 'recTime', timetables.record_synchbox );
+end
+
+if ~isempty(timetables.record_game)
+  newgameevents = euAlign_addTimesToAllTables( ...
+    oldgameevents, 'unityTime', 'recTime', timetables.record_game );
+end
+
+if ~isempty(timetables.record_stim)
+  newstimevents = euAlign_addTimesToAllTables( ...
+    oldstimevents, 'stimTime', 'recTime', times_recorder_stimulator );
+end
+
+
+
+% Fiddle with the frame data table.
+
+if ~isempty(newgameframedata)
+
+  % Clean up gaze timestamps in framedata. The ET timestamps aren't unique
+  % in the original. We can interpolate to find out what they should be.
+
+  if ~isempty(timetables.game_eye)
+    newgameframedata = euAlign_addTimesToTable( newgameframedata, ...
+      'unityTime', 'eyeTime', timetables.game_eye );
+  end
+
+  % Add recorder timestamps to framedata.
+
+  if ~isempty(timetables.record_game)
+    newgameframedata = euAlign_addTimesToTable( newgameframedata, ...
+      'unityTime', 'recTime', timetables.record_game );
+  end
+
+end
+
+
+
+% Fiddle with the gaze data table.
+
+if ~isempty(newgamegazedata)
+
+  % Propagate unity timestamps first.
+
+  if ~isempty(timetables.game_eye)
+    newgamegazedata = euAlign_addTimesToTable( newgamegazedata, ...
+      'eyeTime', 'unityTime', timetables.game_eye );
+
+    % If we have unity timestamps, derive recorder timestamps too.
+    if ~isempty(timetables.record_game)
+      newgamegazedata = euAlign_addTimesToTable( newgamegazedata, ...
+        'unityTime', 'recTime', timetables.record_game );
+    end
+  end
+
+end
+
+
+
+
+% FIXME - Not propagating stimulator timestamps anywhere.
+% FIXME - We should add support for "just stimulator, no recorder" later.
+
+
+% Done.
+end
+
+%
+% This is the end of the file.
