e6468742a29c1383dcbf9c4df3bd9f9053fea0f0
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-07-30 15:53:32 -0500

    Modified guessBadChansFromBandPower to normalize and to perform multiple iterations.

--- libraries/lib-exputils-highlevel/euHLev_guessBadChansFromBandPower.m
+++ libraries/lib-exputils-highlevel/euHLev_guessBadChansFromBandPower.m
@@ -1,27 +1,33 @@
 function badchanmask = euHLev_guessBadChansFromBandPower( ...
-  normband, normtone, bandrange, tonerange )
+  bandpower, tonepower, bandrange, tonerange )
 
 % function badchanmask = euHLev_guessBadChansFromBandPower( ...
-%   normband, normtone, bandrange, tonerange )
+%   bandpower, tonepower, bandrange, tonerange )
 %
-% This processes the output of nlProc_getBandPower(), and decides which
-% channels are good and which are bad.
+% This z-scores the output of nlProc_getBandPower() across channels, and
+% identifies outliers (presumed to be bad channels). Multiple iterations may
+% be performed (removing extreme outliers and recomputing outlier thresholds).
 %
-% "normband" is a nChans x nBands x nTrials matrix containing normalized
-%   total in-band power for each channel, band, and trial. This is expected
-%   to be z-scored across channels (zero mean, standard deviation +/- 1).
-% "normtone" is a nChans x nBands x nTrials matrix containing normalized
-%   tone power (with tone power defined as the maximum in-band component
-%   power divided by median in-band component power). This is expected to be
-%   z-scored across channels (zero mean, standard deviation +/- 1).
-% "bandrange" [ low high ] is the accepted range for in-band power. Specify []
-%   to use defaults. Anything outside this range is considered bad. A value
-%   of NaN for low or high thresholds results in a default value being used.
-% "tonerange" [ low high ] is the accepted range for tone power. specify []
-%   to use defaults. Anything outside this range is considered bad. A value
-%   of NaN for low or high thresholds results in a default value being used.
+% If more iterations are performed than there are rows in "bandrange" or
+% "tonerange", the last row in the relevant matrix is duplicated as needed.
 %
-% "badchanmask" is a 1 x nChans boolean vector that's true for bad channels
+% "bandpower" is a nChans x nBands x nTrials matrix containing total in-band
+%   power for each channel, band, and trial.
+% "tonepower" is a nChans x nBands x nTrials matrix containing normalized
+%   tone power (defined as the maximum in-band component power divided by
+%   the median in-band component power).
+% "bandrange" [ low high ; low high ; ... ] is the accepted range for in-band
+%   power, in standard deviations. Anything outside this range is an outlier.
+%   A value of NaN for a low or high threshold results in a default value
+%   being used. The number of rows indicates the number of pruning passes to
+%   be performed. An empty matrix [] is interpreted as [ NaN NaN ].
+% "tonerange" [ low high ; low high ; ... ] is the accepted range for tone
+%   power, in standard deviations. Anything outside this range is an outlier.
+%   A value of NaN for a low or high threshold results in a default value
+%   being used. The number of rows indicates the number of pruning passes to
+%   be performed. An empty matrix [] is interpreted as [ NaN NaN ].
+%
+% "badchanmask" is a nChans x 1 boolean vector that's true for bad channels
 %   and false otherwise.
 
 
@@ -34,48 +40,104 @@ defaulttonerange = [ -inf 2 ];
 % Number of bands that have to vote "bad" for a channel to be bad.
 minbadcount = 2;
 
+% 'zscore', 'median', or 'twosided'.
+scoremethod = 'twosided';
 
 
 %
-% Set ranges to defaults if necessary.
+% Fill in empty ranges if necessary.
 
-if length(bandrange) ~= 2
+if isempty(bandrange)
   bandrange = [ NaN NaN ];
 end
 
-if length(tonerange) ~= 2
+if isempty(tonerange)
   tonerange = [ NaN NaN ];
 end
-
-bandrange(isnan(bandrange)) = defaultbandrange(isnan(bandrange));
-tonerange(isnan(tonerange)) = defaulttonerange(isnan(tonerange));
 
 
 
 %
 % Get metadata.
 
-chancount = size(normband,1);
-bandcount = size(normband,2);
+chancount = size(bandpower,1);
+bandcount = size(bandpower,2);
 
+passcount = max( size(bandrange,1), size(tonerange,1) );
 
 
 %
 % Figure out how many observations are "bad" and build the "bad channel" mask.
 
 % Collapse trials.
-normband = mean(normband,3);
-normtone = mean(normtone,3);
+bandpower = mean(bandpower,3);
+tonepower = mean(tonepower,3);
 
-% Get bad observations.
-badband = (normband < min(bandrange)) | (normband > max(bandrange));
-badtone = (normtone < min(tonerange)) | (normtone > max(tonerange));
 
-% Collapse bands, to get per-channel bad observation counts.
-badchanmask = sum(badband,2) + sum(badtone,2);
+% Iteratively check for bad trials.
 
-% Compare with the "bad observations" threshold to get the mask.
-badchanmask = (badchanmask >= minbadcount);
+thisbandrange = defaultbandrange;
+thistonerange = defaulttonerange;
+
+badchanmask = false([ chancount, 1 ]);
+
+for pidx = 1:passcount
+
+  % Get this pass's thresholds, retaining the old ones if we're out of rows.
+  if size(bandrange,1) >= pidx
+    thisbandrange = bandrange(pidx,:);
+  end
+  if size(tonerange,1) >= pidx
+    thistonerange = tonerange(pidx,:);
+  end
+
+  % Replace NaN with defaults.
+  thisbandrange(isnan(thisbandrange)) = defaultbandrange(isnan(thisbandrange));
+  thistonerange(isnan(thistonerange)) = defaulttonerange(isnan(thistonerange));
+
+
+  % Get non-bad data and normalize.
+
+  validmask = ~badchanmask;
+
+  if any(validmask)
+
+    % Get valid channels and z-score only across these.
+
+    thisbandpower = bandpower(validmask,:,:);
+    thistonepower = tonepower(validmask,:,:);
+
+    normband = nlProc_normalizeAcrossChannels( thisbandpower, scoremethod );
+    normtone = nlProc_normalizeAcrossChannels( thistonepower, scoremethod );
+% FIXME - Diagnostics.
+disp([ min(normband,[],'all'), max(normband,[],'all'), ...
+min(normtone,[],'all'), max(normtone,[],'all') ]);
+
+
+    % Get outliers.
+    badband = ...
+      (normband < min(thisbandrange)) | (normband > max(thisbandrange));
+    badtone = ...
+      (normtone < min(thistonerange)) | (normtone > max(thistonerange));
+
+    % Collapse bands, to get per-channel outlier counts.
+    thisbadmask = sum(badband,2) + sum(badtone,2);
+
+    % Compare with the "bad observations" threshold to get the mask.
+    thisbadmask = (thisbadmask >= minbadcount);
+
+% FIXME - Diagnostics.
+disp(sprintf( 'xx Pass %d of %d: %d of %d channels (%d bad)', ...
+pidx, passcount, sum(validmask), length(validmask), sum(thisbadmask) ));
+
+    % Update this subset of the global bad channel mask.
+    badchanmask( find(validmask) ) = thisbadmask;
+  end
+
+% FIXME - Diagnostics.
+disp(sprintf( 'xx %d bad channels after pass %d.', sum(badchanmask), pidx ));
+
+end
 
 
 
