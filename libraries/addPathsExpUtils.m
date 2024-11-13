8c6964dd5810e8cabd352da5243a28e986f9acfc
Christopher Thomas <christopher.thomas.webmail@gmail.com>
2024-11-13 09:47:16 -0600

    Created addPaths helper without deprecated 'cjt' suffix.

---
+++ libraries/addPathsExpUtils.m
@@ -0,0 +1,38 @@
+function addPathsExpUtilsCjt
+
+% function addPathsExpUtilsCjt
+%
+% This function detects its own path and adds appropriate child paths to
+% Matlab's search path.
+%
+% No arguments or return value.
+
+
+% Detect the current path.
+
+fullname = which('addPathsExpUtilsCjt');
+[ thisdir fname fext ] = fileparts(fullname);
+
+
+% Add the new paths.
+% (This checks for duplicates, so we don't have to.)
+
+addpath([ thisdir filesep 'lib-exputils-align' ]);
+addpath([ thisdir filesep 'lib-exputils-chris' ]);
+addpath([ thisdir filesep 'lib-exputils-ft' ]);
+addpath([ thisdir filesep 'lib-exputils-highlevel' ]);
+addpath([ thisdir filesep 'lib-exputils-meta' ]);
+addpath([ thisdir filesep 'lib-exputils-plot' ]);
+addpath([ thisdir filesep 'lib-exputils-shims' ]);
+addpath([ thisdir filesep 'lib-exputils-tools' ]);
+addpath([ thisdir filesep 'lib-exputils-use' ]);
+addpath([ thisdir filesep 'lib-exputils-util' ]);
+
+
+% Done.
+
+end
+
+
+%
+% This is the end of the file.
